afd01fad8cd2b804a8720694ca5b4ded3ef01211
jsoques1 <joseph.soques@gmail.com>
2022-07-12 18:31:32 +0200

    add tables management for admin via endpoint admin

--- softdesk/admin.py
+++ softdesk/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from softdesk.models import Projects, Issues, Contributors, Comments
 
-# Register your models here.
+admin.site.register(Projects)
+admin.site.register(Issues)
+admin.site.register(Contributors)
+admin.site.register(Comments)
+
